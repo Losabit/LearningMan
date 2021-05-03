996205a94b022981904ed08ee72354ad397f99b0
Marvin <44065639+Mrenoir@users.noreply.github.com>
2021-05-03 12:32:42 +0200

    Can jump and travels plaform, need to fix gravity , prepare refactoring for map

---
+++ LearningMan/Map/MapEnvironnement.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mirii on 03/05/2021.
+//
+
+#include "MapEnvironnement.h"
---
+++ LearningMan/Map/MapEnvironnement.h
@@ -0,0 +1,14 @@
+//
+// Created by Mirii on 03/05/2021.
+//
+
+#ifndef LEARNINGMAN_MAPENVIRONNEMENT_H
+#define LEARNINGMAN_MAPENVIRONNEMENT_H
+
+
+class MapEnvironnement {
+
+};
+
+
+#endif //LEARNINGMAN_MAPENVIRONNEMENT_H
