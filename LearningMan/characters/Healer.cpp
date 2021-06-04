c68e7c542a34b82b5ea1e81bd211503436b40dda
Marvin <44065639+Mrenoir@users.noreply.github.com>
2021-06-04 11:49:45 +0200

    Heal

---
+++ LearningMan/characters/Healer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mirii on 04/06/2021.
+//
+
+#include "Healer.h"
---
+++ LearningMan/characters/Healer.h
@@ -0,0 +1,14 @@
+//
+// Created by Mirii on 04/06/2021.
+//
+
+#ifndef LEARNINGMAN_HEALER_H
+#define LEARNINGMAN_HEALER_H
+
+
+class Healer {
+
+};
+
+
+#endif //LEARNINGMAN_HEALER_H
