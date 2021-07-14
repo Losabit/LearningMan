0551e08dd8ee01783a90d9bf3da7302a9d506ec2
Marvin <44065639+Mrenoir@users.noreply.github.com>
2021-07-14 07:29:26 +0200

    Add post on GAME and other

--- LearningMan/utils/curlFunction.cpp
+++ LearningMan/utils/curlFunction.cpp
@@ -56,5 +56,32 @@
 //    curl = NULL ;
 //    return 0;
 //}
+//int addGame(std::string start,std::string end,std::string token, int isDead, int playerType, int score, int nbKill, int deathPosition , std::string killer) {
+//    CURL *curl;
+//
+//    std::string  output;
+//    curl_global_init(CURL_GLOBAL_ALL);
+//    curl = curl_easy_init();
+//    CURLcode res;
+//    if (curl) {
+//        curl_easy_setopt(curl, CURLOPT_URL, GAMEURL);
+//        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+//        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+//        std::ostringstream out;
+//        out << "{\"token\": " << token<<R"(,"dateStart":")" << start << R"(" ,"isDead" : )" << isDead  << ", \"playerType\" : " << playerType  <<   ", \"scoreTotal\" :"  << score << ", \"nbKill\": " << nbKill << ", \"deathPosition\" :" << deathPosition<< R"(, "killer":" )" <<  killer   << R"(", "dateEnd":)"   "\""<< end <<    R"(","idMap":)"   << 1  << "}  ";
+//
+//        cout << out.str() << endl;
+//        std::string temp = out.str();
+//        char * postValue =  const_cast<char *>(temp.c_str());
+//        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, postValue );
+//        res = curl_easy_perform(curl);
+//        if (res != CURLE_OK) {
+//            return 1;
+//        }
+//    }
+//    curl = NULL ;
+//    return 0;
+//}
+
 //
 //
